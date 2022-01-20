a22f65f5d9e513459b583f5cad924593c1d9dfe4
tauh33dkhan <tauh33dkhan@gmail.com>
2022-01-21 00:26:26 +0530

    fixing .gitignore

--- env.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  listen: "0.0.0.0",
-  port:	  "9000",
-  mySQLHost: "0.0.0.0",
-  mySQLPort: "3306",
-  mySQLUser: "root",
-  mySQLDB: "vuln_nodejs_app",
-  mySQLPass: "secret"
-}
